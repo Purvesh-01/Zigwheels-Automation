23dac544f8c009ff3f9640d93d94794b15d2f959
Aditya Sarode <AdityaSomnath.Sarode@cognizant.com>
2026-04-30 10:56:23 +0530

    Added ConfigReader class and properties with BaseTest and UsedCarsTest

--- src/main/java/com/zigwheels/utils/ConfigReader.java
+++ src/main/java/com/zigwheels/utils/ConfigReader.java
@@ -1,4 +1,23 @@
 package com.zigwheels.utils;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/main/resources/config.properties");
+            properties.load(fis);
+        } catch (IOException e) {
+            throw new RuntimeException("config.properties file not found!", e);
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
 }
--- src/test/java/test/UsedCarsTest.java
+++ src/test/java/test/UsedCarsTest.java
@@ -1,4 +1,28 @@
 package test;
+import base.BaseTest;
+import com.zigwheels.utils.ConfigReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-public class UsedCarsTest {
+public class UsedCarsTest extends BaseTest {
+
+    @Test
+    public void VerifyHomePage(){
+        String actualUrl = driver.getCurrentUrl();
+        String expectedUrl = ConfigReader.getProperty("baseUrl");
+        Assert.assertEquals(actualUrl,expectedUrl);
+    }
+
+    @Test
+    public void visitUsedCarsPage() {
+        Actions action = new Actions(driver);
+        WebElement more = driver.findElement(By.xpath(ConfigReader.getProperty("moreBtn.xpath")));
+        action.moveToElement(more).perform();
+        driver.findElement(By.xpath(ConfigReader.getProperty("usedCars.xpath"))).click();
+        String usedCarUrl = driver.getCurrentUrl();
+        Assert.assertEquals(usedCarUrl,ConfigReader.getProperty("usedCars.url"));
+    }
 }
